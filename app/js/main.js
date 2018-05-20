e742cd78740863cc93c708c21d3b75f8ad103fa3
Grzegorz Nęcka <strzypior@gmail.com>
2018-05-20 20:13:34 +0200

    2

---
+++ app/js/main.js
@@ -0,0 +1,78 @@
+$(function () {
+
+ // Add smooth scrolling to all links
+  $("a").on('click', function(event) {
+
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+   
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    } // End if
+  });
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+//scroll navbara ///
+
+
+    $(window).scroll(function () {
+        changeBackground()
+    })
+
+    function changeBackground() {
+        let navHeight = $('#main-nav').outerHeight();
+        //outerHeight pobiera wysokość el z paddingiem
+
+        let currentPosition = $(window).scrollTop();
+
+        if (currentPosition >= navHeight) {
+            $('#main-nav').addClass('scrolled');
+        } else {
+            $('#main-nav').removeClass('scrolled');
+        }
+        console.log(currentPosition)
+
+        /*     console.log(navHeight)*/
+
+
+    } //end changeBacground
+
+}); //end quert
